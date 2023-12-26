22dfcf863d96376261089b92632e023752c52880
mozzzqa <mozetron@ya.ru>
2023-12-26 16:52:44 +0300

    HF1

---
+++ src/Main2.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Kotlin"
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+
+    var count = 1
+
+    while (count <= 5) {
+        count = count + 1
+        println("Hello, " + name + "!")
+    }
+}
