0a9f597687fd1c780f340597ca17f6386b5a74ef
mozzzqa <mozetron@ya.ru>
2023-12-29 18:09:27 +0300

    HF 6

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -1,9 +1,13 @@
 fun main() {
-    val firemen = arrayOf("Pidr","Soska","Soska")
-    var firemanNo = 0
 
-    while (firemanNo < 3) {
-        println("Firemen number $firemanNo is ${firemen[firemanNo]}")
-        firemanNo = firemanNo + 1
+    val fruits = arrayOf("Eba","Banana","Blueberry","Pomegranate","Cherry")
+    val index = arrayOf(1,4,2,3)
+    var x = 0
+    var count: Int
+
+    while (x < 4) {
+        count = index[x]
+        println("Fruit = ${fruits[count]}")
+        x = x + 1
     }
 }
