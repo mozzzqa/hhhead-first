a5140979c1992c6140a24a3057de7b1e6e46e12e
mozzz <eduard.lodyga@rt.ru>
2024-04-09 10:23:28 +0300

    1 полиморфизм

---
+++ src/AnimalS.kt
@@ -0,0 +1,16 @@
+import second_class.Vet
+
+fun main(args: Array<String>) {
+    val animals = arrayOf(Hippo(), wolf())
+    for (item in animals) {
+        item.roam()
+        item.eat()
+    }
+
+    val vet = Vet()
+    val Wolf = wolf()
+    val hippo = Hippo()
+    vet.giveShot(Wolf)
+    vet.giveShot(hippo)
+
+}
---
+++ src/second_class/2nd_class.kt
@@ -0,0 +1,9 @@
+package second_class
+
+import AnimalS_class
+
+class Vet {
+    fun giveShot(animal: AnimalS_class) {
+        animal.makeNoise()
+    }
+}
